40f91a5a8ce83ee7a9c593fa07ae908eec29ce13
Unknown <Isaac.overlander@gmail.com>
2018-08-22 16:26:19 -0500

    Added myApp.config and created routes to the views

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,11 @@
-const myApp = angular.module('myApp', [ngRoute]);
+const myApp = angular.module('myApp', ['ngRoute']);
 
+myApp.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'views/entry.html'
+    }).when('/projects', {
+        templateUrl: 'views/projects.html'
+    }).otherwise({
+        templateUrl: 'views/404.html'
+    });
+})
