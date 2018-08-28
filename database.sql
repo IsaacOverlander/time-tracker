13845eeb9ff0aab3c1d4717a6c7040756c00989c
Unknown <Isaac.overlander@gmail.com>
2018-08-28 16:43:09 -0500

    Fixed database.sql

--- database.sql
+++ database.sql
@@ -19,11 +19,11 @@ CREATE TABLE "projects" (
 INSERT INTO "projects" ("name")
 VALUES('Time Tracker');
 
-INSERT INTO "entries" ("task", "date", "time")
-VALUES ('created entries table', '8/24/2018', '30');
+INSERT INTO "entries" ("task", "date", "hours", "minutes", "start", "end", "project_id")
+VALUES ('created entries table', '8/24/2018', '1', '2', '12:30', '12:40', '1');
 
-SELECT * FROM "entries" JOIN "projects" ON "projects"."id" = "entries"."project_id";
-DELETE FROM "entries" WHERE "id" = 23;
+SELECT * FROM "entries" JOIN "projects" ON "projects"."projectid" = "entries"."project_id";
+DELETE FROM "entries" WHERE "id" = 1;
 
 SELECT "projects"."name", "entries"."project_id", SUM("entries"."hours") as hours, SUM("entries"."minutes") as minutes FROM "entries"
 JOIN "projects" ON "projects"."projectid" = "entries"."project_id" GROUP BY "projects"."name";
